4d4f6d3276b2bea3b47575cf53c78ac14dc4af51
unknown <waldidefari63@gmail.com>
2021-09-26 22:21:07 +0700

    TugasIMA.java

---
+++ wnwdwni/TugasIMA.java
@@ -0,0 +1,9 @@
+public class TugasIMA{
+
+public static void main(String[] args){
+	System.out.println("Nama:Waldi");
+	System.out.println("NIM:6706210061");
+	System.out.println("Kelas:45-01");
+    }
+
+}
